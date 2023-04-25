242733892eaaca12bec5fe1ef056335a58c743f4
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-04-25 10:38:41 +0000

    fs erro checkpoint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+import { HashedModuleIdsPlugin } from "webpack";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  console.log("Hello Rigo from the console! ");
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["jogger", "racoon", "dog", "driver", "clown", "pine cone"];
+  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let possession = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actionIndx = Math.floor(Math.random() * action.length);
+  let possesionIndx = Math.floor(Math.random() * possession.length);
+  let whereIndx = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjIndx] +
+    " " +
+    action[actionIndx] +
+    " " +
+    possession[possesionIndx] +
+    " " +
+    where[whereIndx]
+  );
 };
